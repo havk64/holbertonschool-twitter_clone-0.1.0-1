fac2ad6d49adcd639a971007c6649da1c9b84015
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-04-02 02:51:05 -0300

    Consolidating and compressing js files

--- js/reply.js
+++ js/reply.js
@@ -2,14 +2,13 @@
 document.addEventListener("DOMContentLoaded", function() {
 // document.addEventListener(function() {    
 	var reply = document.querySelectorAll('.button-reply'); // find all elements with reply class
-	var r = 0, r_length = reply.length; // create & set new vars to 0 and to number of reply btns (length)
-		for (r; r < r_length; r++) { // iterates through all reply btns adding event listeners to each
-		reply[r].addEventListener('click', function() { // when click is heard do the following:
+			for (var i = 0; i < reply.length; i++) { // iterates through all reply btns adding event listeners to each
+		reply[i].addEventListener('click', function() { // when click is heard do the following:
 		var parent = this.closest('.post-status-button-div');  // set target 1 for toggling
 		var post = this.parentNode.nextElementSibling; // set target 2 for toggle
 		post.toggle(); 
 		parent.toggle();
-		});
+		}, false );
 	}
 });
 
